bd4e440eb0da1d712aded196be38803d02946bca
Moroz9 <126159245+Moroz9@users.noreply.github.com>
2023-05-24 15:06:05 +0400

    refactor  ProfileVC

--- RegistrationApp/Screens/Profile/ProfileVC.swift
+++ RegistrationApp/Screens/Profile/ProfileVC.swift
@@ -8,18 +8,13 @@
 import UIKit
 
 final class ProfileVC: UIViewController {
-    // MARK: - Let \ Var
+    // MARK: - Properties
     private let contentViewBack: UIView = {
-        let imageView = UIView()
-        imageView.backgroundColor = Color.backgroundView
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
+        let view = UIView()
+        view.backgroundColor = Color.backgroundView
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
     }()
-    private let storage: UserDefaultsManagerProtocol = UserDefaultsManager()
-    private var dataSource = [Section]()
-    private var profilePersons = PersonsModel.exampleList
-    private var profileUpLoadItemButtonModel = UpLoadItemButtonModel.exampleList
-    private var profileButtonSettings = ProfileButtonSettingsModel.exampleList
     
     private let tableView: UITableView = {
         let tableView = UITableView()
@@ -27,73 +22,68 @@ final class ProfileVC: UIViewController {
         tableView.separatorStyle = .none
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
-        
     }()
     
-    // MARK: - Lifecycle func
+    private var dataSource = [Section]()
+    private var profilePersons = PersonsModel.exampleList
+    private var profileUpLoadItemButtonModel = UpLoadItemButtonModel.exampleList
+    private var profileButtonSettings = ProfileButtonSettingsModel.exampleList
+    
+    private let storage: UserDefaultsManagerProtocol = UserDefaultsManager()
+    
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
-        setProfileButtonSettingsConstrains()
         createCustomNavigationBar()
         setData()
         dataSource = createDataSource()
     }
     
-    // MARK: - Flow func
+    // MARK: - Setup
     
     private func setupViews() {
         view.addSubview(contentViewBack)
         view.addSubview(tableView)
-        let customTitleView = createCustomTitleView(contactName: Text.textProfile )
+        
+        let customTitleView = createCustomTitleView(contactName: Text.textProfile)
         navigationItem.titleView = customTitleView
         
-    tableView.register(ProfileChangePhotoViewCell.self, forCellReuseIdentifier: CellIdentifier.idProfileChangePhotoCell)
-    tableView.register(ButtonUpLoadTableViewCell.self, forCellReuseIdentifier:
-        CellIdentifier.idProfileButtonCell)
-    tableView.register(ProfileButtonSettingsViewCell.self, forCellReuseIdentifier: CellIdentifier.idProfileButtonSettingsCell)
+        tableView.register(ProfileChangePhotoViewCell.self, forCellReuseIdentifier: CellIdentifier.idProfileChangePhotoCell)
+        tableView.register(ButtonUpLoadTableViewCell.self, forCellReuseIdentifier: CellIdentifier.idProfileButtonCell)
+        tableView.register(ProfileButtonSettingsViewCell.self, forCellReuseIdentifier: CellIdentifier.idProfileButtonSettingsCell)
+        
+        NSLayoutConstraint.activate([
+            contentViewBack.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentViewBack.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            contentViewBack.topAnchor.constraint(equalTo: view.topAnchor),
+            contentViewBack.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
     
     private func setData() {
-        tableView.reloadData()
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.reloadData()
     }
     
-    func showPicker() {
+    // MARK: - Helper Methods
+    
+    private func showPicker() {
         let picker = UIImagePickerController()
         picker.sourceType = .photoLibrary
         picker.allowsEditing = true
         picker.delegate = self
         present(picker, animated: true, completion: nil)
     }
-}
-
-// MARK: - UIImagePickerControllerDelegate
-
-extension ProfileVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-        var chosenImage = UIImage()
-        if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
-            chosenImage = image
-        } else if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            chosenImage = image
-        }
-        
-        guard let data = chosenImage.jpegData(compressionQuality: 0.5) else { return }
-            let encoded = try? PropertyListEncoder().encode(data)
-            storage.set(encoded, forKey: .imagePhoto)
-        
-        picker.dismiss(animated: true, completion: nil)
-        tableView.reloadData()
-    }
-}
-
-// MARK: - ProfileVC
-
-private extension ProfileVC {
-    func createDataSource() -> [Section] {
+    
+    private func createDataSource() -> [Section] {
         var sections = [Section]()
         sections.append(Section.idProfileChangePhotoCell(items: profilePersons))
         sections.append(Section.idProfileButtonCell(items: profileUpLoadItemButtonModel))
@@ -102,69 +92,74 @@ private extension ProfileVC {
     }
 }
 
-// MARK: - UITableViewDelegate
+// MARK: - UITableViewDelegate & UITableViewDataSource
 
 extension ProfileVC: UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        dataSource.count
+        return dataSource.count
     }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch dataSource[section] {
-        case .idProfileChangePhotoCell(let items): return items.count
-        case .idProfileButtonCell(let items): return items.count
-        case .idProfileButtonSettingsCell(let items): return items.count
+        let currentSection = dataSource[section]
+        switch currentSection {
+        case .idProfileChangePhotoCell(let items):
+            return items.count
+        case .idProfileButtonCell(let items):
+            return items.count
+        case .idProfileButtonSettingsCell(let items):
+            return items.count
         }
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let section = dataSource[indexPath.section]
-        switch section {
+        let currentSection = dataSource[indexPath.section]
+        
+        switch currentSection {
         case .idProfileChangePhotoCell(let items):
-            let cell = tableView.dequeueReusableCell(
-                withIdentifier: CellIdentifier.idProfileChangePhotoCell,
-                for: indexPath
-            ) as? ProfileChangePhotoViewCell
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier.idProfileChangePhotoCell, for: indexPath) as? ProfileChangePhotoViewCell else {
+                return UITableViewCell()
+            }
             let item = items[indexPath.row]
-            cell?.setData(item)
-            return cell ?? UITableViewCell()
+            cell.setData(item)
+            return cell
             
-        case .idProfileButtonCell(items: let items):
-            let cell = tableView.dequeueReusableCell(
-                withIdentifier: CellIdentifier.idProfileButtonCell,
-                for: indexPath
-            ) as? ButtonUpLoadTableViewCell
-            _ = items[indexPath.row]
-            return cell ?? UITableViewCell()
-            
-        case .idProfileButtonSettingsCell(items: let items):
-            let cell = tableView.dequeueReusableCell(
-                withIdentifier: CellIdentifier.idProfileButtonSettingsCell,
-                for: indexPath
-            ) as? ProfileButtonSettingsViewCell
+        case .idProfileButtonCell(let items):
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier.idProfileButtonCell, for: indexPath) as? ButtonUpLoadTableViewCell else {
+                return UITableViewCell()
+            }
             let item = items[indexPath.row]
-            cell?.setData(item)
-            return cell ?? UITableViewCell()
+            return cell
+            
+        case .idProfileButtonSettingsCell(let items):
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier.idProfileButtonSettingsCell, for: indexPath) as? ProfileButtonSettingsViewCell else {
+                return UITableViewCell()
+            }
+            let item = items[indexPath.row]
+            cell.setData(item)
+            return cell
         }
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: false)
+        
         switch indexPath {
         case [0, 0]:
             showPicker()
         case [1, 0]:
-            print("at ", [indexPath.section], [indexPath.row])
+            print("at", [indexPath.section], [indexPath.row])
         case [2, 0]:
-            print("at ", [indexPath.section], [indexPath.row])
+            print("at", [indexPath.section], [indexPath.row])
         case [2, 1]:
-            print("at ", [indexPath.section], [indexPath.row])
+            print("at", [indexPath.section], [indexPath.row])
         case [2, 2]:
-            print("at ", [indexPath.section], [indexPath.row])
+            print("at", [indexPath.section], [indexPath.row])
         case [2, 3]:
-            print("at ", [indexPath.section], [indexPath.row])
+            print("at", [indexPath.section], [indexPath.row])
         case [2, 4]:
-            print("at ", [indexPath.section], [indexPath.row])
+            print("at", [indexPath.section], [indexPath.row])
         case [2, 5]:
-            print("at ", [indexPath.section], [indexPath.row])
+            print("at", [indexPath.section], [indexPath.row])
         case [2, 6]:
             let controller = AuthenticationVC()
             let navController = UINavigationController(rootViewController: controller)
@@ -176,21 +171,28 @@ extension ProfileVC: UITableViewDelegate, UITableViewDataSource {
     }
 }
 
-// MARK: - ProfileVC
+// MARK: - UIImagePickerControllerDelegate
 
-extension ProfileVC {
-    private func setProfileButtonSettingsConstrains() {
-        view.addSubview(tableView)
-        NSLayoutConstraint.activate([
-            contentViewBack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
-            contentViewBack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
-            contentViewBack.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
-            contentViewBack.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
-            
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
-        ])
+extension ProfileVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        var chosenImage = UIImage()
+        
+        if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            chosenImage = image
+        } else if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            chosenImage = image
+        }
+        
+        guard let data = chosenImage.jpegData(compressionQuality: 0.5) else {
+            picker.dismiss(animated: true, completion: nil)
+            return
+        }
+        
+        let encoded = try? PropertyListEncoder().encode(data)
+        storage.set(encoded, forKey: .imagePhoto)
+        
+        picker.dismiss(animated: true) {
+            self.tableView.reloadData()
+        }
     }
 }
